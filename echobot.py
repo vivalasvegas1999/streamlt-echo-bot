8faf99eb04a0788f18b0d09c8a0f69b4d12962b5
vivalasvegas1999 <70182189+vivalasvegas1999@users.noreply.github.com>
2026-03-05 15:26:02 -0800

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,16 +1,14 @@
 import streamlit as st
-import numpy as np
 
-with st.chat_message("assistant"):
-    st.write("Hello human")
-    st.bar_chart(np.random.randn(30, 3))
-    st.line_chart(np.random.randn(10, 3))
+st.title("Echo Bot")
 
-with st.chat_message("user"):
-    st.write("Hello 👋")
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
 
