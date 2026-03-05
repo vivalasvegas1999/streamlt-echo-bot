56beafdb6f8afc934ef2e00d00e50082182ab057
vivalasvegas1999 <70182189+vivalasvegas1999@users.noreply.github.com>
2026-03-05 15:20:48 -0800

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -5,3 +5,12 @@ with st.chat_message("assistant"):
     st.write("Hello human")
     st.bar_chart(np.random.randn(30, 3))
     st.line_chart(np.random.randn(10, 3))
+
+with st.chat_message("user"):
+    st.write("Hello 👋")
+
+
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
+
